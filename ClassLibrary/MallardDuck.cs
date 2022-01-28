dd86612dd43a8d81be34a3eed95e85f3e614b2ae
Tesl1k <danildavydov161@mail.ru>
2022-01-28 10:52:22 +0300

    LR01

---
+++ ClassLibrary/MallardDuck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MallardDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Displya1 MallardDuck " + this.GetType();
+        }
+
+    }
+}
---
+++ Duck/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Ducks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck duck1 = new MallardDuck();
+            RedheadDuck duck2 = new RedheadDuck();
+
+            Duck[] ducks = new Duck[] { duck1, duck2 };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Quack());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Display());
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
